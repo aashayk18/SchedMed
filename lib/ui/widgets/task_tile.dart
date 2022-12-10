ff4ee37e1e69972beec99e1ff8efa514ec920668
Aashay Kulkarni <72932792+aashayk18@users.noreply.github.com>
2022-12-10 18:24:15 +0530

    Update task_tile.dart

--- lib/ui/widgets/task_tile.dart
+++ lib/ui/widgets/task_tile.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -6,16 +7,16 @@ import '../theme.dart';
 
 class TaskTile extends StatelessWidget {
   final Task? task;
-  const TaskTile(this.task, {super.key});
+  TaskTile(this.task);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
+      padding: EdgeInsets.symmetric(horizontal: 20),
       width: MediaQuery.of(context).size.width,
-      margin: const EdgeInsets.only(bottom: 12),
+      margin: EdgeInsets.only(bottom: 12),
       child: Container(
-        padding: const EdgeInsets.all(16),
+        padding: EdgeInsets.all(16),
         //  width: SizeConfig.screenWidth * 0.78,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(16),
@@ -29,13 +30,13 @@ class TaskTile extends StatelessWidget {
                 Text(
                   task?.title ?? "",
                   style: GoogleFonts.lato(
-                    textStyle: const TextStyle(
+                    textStyle: TextStyle(
                         fontSize: 16,
                         fontWeight: FontWeight.bold,
                         color: Colors.white),
                   ),
                 ),
-                const SizedBox(
+                SizedBox(
                   height: 12,
                 ),
                 Row(
@@ -46,7 +47,7 @@ class TaskTile extends StatelessWidget {
                       color: Colors.grey[200],
                       size: 18,
                     ),
-                    const SizedBox(width: 4),
+                    SizedBox(width: 4),
                     Text(
                       "${task!.startTime} - ${task!.endTime}",
                       style: GoogleFonts.lato(
@@ -56,7 +57,7 @@ class TaskTile extends StatelessWidget {
                     ),
                   ],
                 ),
-                const SizedBox(height: 12),
+                SizedBox(height: 12),
                 Text(
                   task?.note ?? "",
                   style: GoogleFonts.lato(
@@ -67,7 +68,7 @@ class TaskTile extends StatelessWidget {
             ),
           ),
           Container(
-            margin: const EdgeInsets.symmetric(horizontal: 10),
+            margin: EdgeInsets.symmetric(horizontal: 10),
             height: 60,
             width: 0.5,
             color: Colors.grey[200]!.withOpacity(0.7),
@@ -77,7 +78,7 @@ class TaskTile extends StatelessWidget {
             child: Text(
               task!.isCompleted == 1 ? "COMPLETED" : "TODO",
               style: GoogleFonts.lato(
-                textStyle: const TextStyle(
+                textStyle: TextStyle(
                     fontSize: 10,
                     fontWeight: FontWeight.bold,
                     color: Colors.white),
