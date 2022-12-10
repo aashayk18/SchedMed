d6a9303ec07ea78f3f1c13e32c61dd9348c5f43e
Aashay Kulkarni <72932792+aashayk18@users.noreply.github.com>
2022-12-10 18:23:33 +0530

    Update notified_page.dart

--- lib/ui/notified_page.dart
+++ lib/ui/notified_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -8,32 +9,36 @@ class NotifiedPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Get.isDarkMode ? Colors.grey[600] : Colors.white,
-        leading: IconButton(
-            onPressed: () => Get.back(),
-            icon: const Icon(Icons.arrow_back_ios),
-            color: Get.isDarkMode ? Colors.white : Colors.grey),
-        title: Text(
-          label.toString().split("|")[0],
-          style: const TextStyle(color: Colors.black),
+      appBar: AppBar (
+        backgroundColor: Get.isDarkMode?Colors.grey[600]:Colors.white,
+        leading: IconButton (
+          onPressed: ()=>Get.back(),
+          icon: Icon(Icons.arrow_back_ios),
+          color: Get.isDarkMode?Colors.white:Colors.grey
+        ),
+        title: Text(this.label.toString().split("|")[0],
+            style: TextStyle(
+              color: Colors.black
+            ),
         ),
       ),
       body: Center(
         child: Container(
-            height: 400,
-            width: 300,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: Get.isDarkMode ? Colors.white : Colors.grey[400]),
-            child: Center(
-              child: Text(
-                label.toString().split("|")[1],
-                style: TextStyle(
-                    color: Get.isDarkMode ? Colors.black : Colors.white,
-                    fontSize: 30),
+          height: 400,
+          width: 300,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Get.isDarkMode?Colors.white:Colors.grey[400]
+          ),
+          child: Center(
+            child: Text ( this.label.toString().split("|")[1],
+              style: TextStyle(
+                  color: Get.isDarkMode?Colors.black:Colors.white,
+                fontSize: 30
               ),
-            )),
+            ),
+          )
+        ),
       ),
     );
   }
