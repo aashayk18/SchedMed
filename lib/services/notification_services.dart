647562f288e68325d7d2fc0ddc3418704a59bfd6
Aashay Kulkarni <72932792+aashayk18@users.noreply.github.com>
2022-12-10 18:25:13 +0530

    Update notification_services.dart

--- lib/services/notification_services.dart
+++ lib/services/notification_services.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
-
+import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_native_timezone/flutter_native_timezone.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
@@ -15,12 +17,20 @@ class NotifyHelper {
 
   initializeNotification() async {
     _configureLocalTimezone();
+    // final IOSInitializationSettings initializationSettingsIOS =
+    // IOSInitializationSettings(
+    //     requestSoundPermission: false,
+    //     requestBadgePermission: false,
+    //     requestAlertPermission: false,
+    //     onDidReceiveLocalNotification: onDidReceiveLocalNotification
+    // );
 
-    const AndroidInitializationSettings initializationSettingsAndroid =
+    final AndroidInitializationSettings initializationSettingsAndroid =
         AndroidInitializationSettings("appicon");
 
-    const InitializationSettings initializationSettings =
+    final InitializationSettings initializationSettings =
         InitializationSettings(
+      // iOS: initializationSettingsIOS
       android: initializationSettingsAndroid,
     );
     await flutterLocalNotificationsPlugin.initialize(initializationSettings,
@@ -31,12 +41,16 @@ class NotifyHelper {
     if (kDebugMode) {
       print("doing test");
     }
-    var androidPlatformChannelSpecifics = const AndroidNotificationDetails(
+    var androidPlatformChannelSpecifics =
+
+    const AndroidNotificationDetails(
         'your channel id', 'your channel name',
         importance: Importance.max, priority: Priority.high);
 
+    //var iOSPlatformChannelSpecifics = new IOSNotificationDetails();
+
     var platformChannelSpecifics =
-        NotificationDetails(android: androidPlatformChannelSpecifics);
+      new NotificationDetails(android: androidPlatformChannelSpecifics);
     await flutterLocalNotificationsPlugin.show(
       0,
       title,
@@ -52,22 +66,24 @@ class NotifyHelper {
         task.title,
         task.note,
         _convertTime(hour, minutes),
+        //tz.TZDateTime.now(tz.local).add(const Duration(seconds: 5)),
         const NotificationDetails(
-            android: AndroidNotificationDetails(
-                'your channel id', 'your channel name')),
+            android: AndroidNotificationDetails('your channel id',
+                'your channel name')),
         androidAllowWhileIdle: true,
         uiLocalNotificationDateInterpretation:
-            UILocalNotificationDateInterpretation.absoluteTime,
+        UILocalNotificationDateInterpretation.absoluteTime,
         matchDateTimeComponents: DateTimeComponents.time,
-        payload: "${task.title}|" "${task.note}|");
+      payload: "${task.title}|"+"${task.note}|"
+    );
   }
 
   tz.TZDateTime _convertTime(int hour, int minutes) {
     final tz.TZDateTime now = tz.TZDateTime.now(tz.local);
     tz.TZDateTime scheduleDate =
         tz.TZDateTime(tz.local, now.year, now.month, now.day, hour, minutes);
-    if (scheduleDate.isBefore(now)) {
-      scheduleDate = scheduleDate.add(const Duration(days: 1));
+    if(scheduleDate.isBefore(now)) {
+      scheduleDate = scheduleDate.add(const Duration(days:1));
     }
     return scheduleDate;
   }
@@ -80,21 +96,16 @@ class NotifyHelper {
 
   Future selectNotification(String? payload) async {
     if (payload != null) {
-      if (kDebugMode) {
-        print('notification payload: $payload');
-      }
+      print('notification payload: $payload');
     } else {
-      if (kDebugMode) {
-        print("Notification Done");
-      }
+      print("Notification Done");
     }
 
     if (payload == "Theme Changed") {
-      if (kDebugMode) {
-        print("Nothing to navigate to");
-      }
+      print("Nothing to navigate to");
     } else {
-      Get.to(() => NotifiedPage(label: payload));
+      Get.to(()=>NotifiedPage(label:payload));
     }
+
   }
 }
