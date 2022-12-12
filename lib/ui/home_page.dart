7746c2c31cc38cb808059694598ea825dce2056a
Ananya M. Jha <80632955+Ananya-Jha1208@users.noreply.github.com>
2022-12-13 01:50:22 +0530

    Removed comment

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -28,7 +28,6 @@ class HomePageState extends State<HomePage> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     notifyHelper = NotifyHelper();
     notifyHelper.initializeNotification();
