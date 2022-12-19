649db32fb8fe977be74cd0dfe172bb37cfa7e999
Aashay Kulkarni <72932792+aashayk18@users.noreply.github.com>
2022-12-19 17:55:07 +0530

    Update task_tile.dart

--- lib/ui/widgets/task_tile.dart
+++ lib/ui/widgets/task_tile.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
 import '../../models/task.dart';
 import '../theme.dart';
 
