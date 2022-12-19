4a285a79bfb6af9494c06b5581ce782249f6b4ca
Aashay Kulkarni <72932792+aashayk18@users.noreply.github.com>
2022-12-19 14:55:58 +0530

    Update theme_services.dart

--- lib/services/theme_services.dart
+++ lib/services/theme_services.dart
@@ -5,11 +5,11 @@ import 'package:get/get.dart';
 class ThemeService {
   final _box = GetStorage();
   final _key = 'isDarkMode';
-_saveThemToBox(bool isDarkMode)=>_box.write(_key, isDarkMode);
-  bool _loadThemeFromBox()=>_box.read(_key)??false;
-  ThemeMode get theme=> _loadThemeFromBox()?ThemeMode.dark:ThemeMode.light;
+  _saveThemToBox(bool isDarkMode) => _box.write(_key, isDarkMode);
+  bool _loadThemeFromBox() => _box.read(_key) ?? false;
+  ThemeMode get theme => _loadThemeFromBox() ? ThemeMode.dark : ThemeMode.light;
   void switchTheme() {
-    Get.changeThemeMode(_loadThemeFromBox()?ThemeMode.light:ThemeMode.dark);
+    Get.changeThemeMode(_loadThemeFromBox() ? ThemeMode.light : ThemeMode.dark);
     _saveThemToBox(!_loadThemeFromBox());
   }
 }
