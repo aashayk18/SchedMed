61114c3ac8b706ecc2f2a5189e41176e46c5db00
Aashay Kulkarni <72932792+aashayk18@users.noreply.github.com>
2022-12-19 18:08:00 +0530

    Update notification_services.dart

--- lib/services/notification_services.dart
+++ lib/services/notification_services.dart
@@ -1,1 +1,100 @@
+import 'package:flutter/foundation.dart';
 
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_native_timezone/flutter_native_timezone.dart';
+import 'package:get/get.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+
+import '../models/task.dart';
+import '../ui/notified_page.dart';
+
+class NotifyHelper {
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin(); //
+
+  initializeNotification() async {
+    _configureLocalTimezone();
+
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings("appicon");
+
+    const InitializationSettings initializationSettings =
+        InitializationSettings(
+      android: initializationSettingsAndroid,
+    );
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: selectNotification);
+  }
+
+  displayNotification({required String title, required String body}) async {
+    if (kDebugMode) {
+      print("doing test");
+    }
+    var androidPlatformChannelSpecifics = const AndroidNotificationDetails(
+        'your channel id', 'your channel name',
+        importance: Importance.max, priority: Priority.high);
+
+    var platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      title,
+      body,
+      platformChannelSpecifics,
+      payload: title,
+    );
+  }
+
+  scheduledNotification(int hour, int minutes, Task task) async {
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+        task.id!.toInt(),
+        task.title,
+        task.note,
+        _convertTime(hour, minutes),
+        const NotificationDetails(
+            android: AndroidNotificationDetails(
+                'your channel id', 'your channel name')),
+        androidAllowWhileIdle: true,
+        uiLocalNotificationDateInterpretation:
+            UILocalNotificationDateInterpretation.absoluteTime,
+        matchDateTimeComponents: DateTimeComponents.time,
+        payload: "${task.title}|" "${task.note}|");
+  }
+
+  tz.TZDateTime _convertTime(int hour, int minutes) {
+    final tz.TZDateTime now = tz.TZDateTime.now(tz.local);
+    tz.TZDateTime scheduleDate =
+        tz.TZDateTime(tz.local, now.year, now.month, now.day, hour, minutes);
+    if (scheduleDate.isBefore(now)) {
+      scheduleDate = scheduleDate.add(const Duration(days: 1));
+    }
+    return scheduleDate;
+  }
+
+  Future<void> _configureLocalTimezone() async {
+    tz.initializeTimeZones();
+    final String timeZone = await FlutterNativeTimezone.getLocalTimezone();
+    tz.setLocalLocation(tz.getLocation(timeZone));
+  }
+
+  Future selectNotification(String? payload) async {
+    if (payload != null) {
+      if (kDebugMode) {
+        print('notification payload: $payload');
+      }
+    } else {
+      if (kDebugMode) {
+        print("Notification Done");
+      }
+    }
+
+    if (payload == "Theme Changed") {
+      if (kDebugMode) {
+        print("Nothing to navigate to");
+      }
+    } else {
+      Get.to(() => NotifiedPage(label: payload));
+    }
+  }
+}
