3f98fb368c3fd1833c0f4f8b710a3c767310de0f
Aashay Kulkarni <72932792+aashayk18@users.noreply.github.com>
2023-03-04 17:17:09 +0530

    Added new buttons and related functions

--- lib/ui/notified_page.dart
+++ lib/ui/notified_page.dart
@@ -1,47 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_to_do_app/ui/theme.dart';
+import 'package:flutter_to_do_app/ui/widgets/button.dart';
+import 'package:flutter_to_do_app/ui/widgets/button5.dart';
+import 'package:flutter_to_do_app/ui/widgets/button6.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../controllers/task_controller.dart';
+import '../models/task.dart';
+
 class NotifiedPage extends StatelessWidget {
   final String? label;
-  const NotifiedPage({Key? key, required this.label}) : super(key: key);
+  NotifiedPage({Key? key, required this.label}) : super(key: key);
+  final _taskController = Get.put(TaskController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Get.isDarkMode ? Colors.grey[600] : Colors.white,
-        leading: IconButton(
-            onPressed: () => Get.back(),
-            icon: const Icon(Icons.arrow_back_ios),
-            color: Get.isDarkMode ? Colors.white : Colors.grey),
-        title: Text(
-          label.toString().split("|")[0],
-          style: const TextStyle(color: Colors.black),
+        appBar: AppBar(
+          backgroundColor: Get.isDarkMode ? Colors.grey[600] : Colors.white,
+          leading: IconButton(
+              onPressed: () => Get.back(),
+              icon: const Icon(Icons.arrow_back_ios),
+              color: Get.isDarkMode ? Colors.white : Colors.grey),
+          title: Text(
+            label.toString().split("|")[0],
+            style: const TextStyle(color: Colors.black),
+          ),
         ),
-      ),
-      body: Center(
-        child: Container(
+        body: Center(
+          child: Container(
             height: 400,
             width: 350,
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(20),
                 color: Get.isDarkMode ? Colors.white : Colors.green[900]),
             child: Container(
-              child: Row(
-                children: [Expanded(
+                child: Row(
+              children: [
+                Expanded(
                   child: Column(
                     children: [
                       Padding(
                         padding: const EdgeInsets.only(top: 8.0),
-                        child: Text(
-                          label.toString().split("|")[0],
-                          style: GoogleFonts.alata(
-                            textStyle: TextStyle(color: Get.isDarkMode ? Colors.black : Colors.white,
-                                fontSize: 50, fontWeight: FontWeight.bold),
-                          )
-                        ),
+                        child: Text(label.toString().split("|")[0],
+                            style: GoogleFonts.alata(
+                              textStyle: TextStyle(
+                                  color: Get.isDarkMode
+                                      ? Colors.black
+                                      : Colors.white,
+                                  fontSize: 50,
+                                  fontWeight: FontWeight.bold),
+                            )),
                       ),
                       SizedBox(height: 40),
                       Padding(
@@ -49,7 +60,10 @@ class NotifiedPage extends StatelessWidget {
                         child: Text(
                           "Instructions:",
                           style: GoogleFonts.alata(
-                              textStyle: TextStyle(color: Get.isDarkMode ? Colors.black : Colors.white,
+                              textStyle: TextStyle(
+                                  color: Get.isDarkMode
+                                      ? Colors.black
+                                      : Colors.white,
                                   fontSize: 23)),
                         ),
                       ),
@@ -59,16 +73,95 @@ class NotifiedPage extends StatelessWidget {
                         child: Text(
                           label.toString().split("|")[1],
                           style: GoogleFonts.alata(
-                              textStyle: TextStyle(color: Get.isDarkMode ? Colors.black : Colors.white,
+                              textStyle: TextStyle(
+                                  color: Get.isDarkMode
+                                      ? Colors.black
+                                      : Colors.white,
                                   fontSize: 25)),
                         ),
                       ),
+                      SizedBox(
+                        height: 20
+                      ),
+                      SizedBox(
+                        width: 120,
+                        child: CompleteButton(label: "Completed", onTap: () {}),
+                      ),
+                      SizedBox(
+                          height: 20
+                      ),
+                      SizedBox (
+                        width: 90
+                      ),
+                      SizedBox (
+                        width: 120,
+                        child: IgnoreButton(label: "Ignore", onTap: () {}),
+                      )
                     ],
                   ),
                 ),
               ],
             )),
-      ),
-    ));
+          ),
+        ));
+  }
+
+  _showBottomSheet(BuildContext context, Task task) {
+    Get.bottomSheet(
+      Container(
+          padding: const EdgeInsets.only(top: 4),
+          child: Column(children: [
+            Container(
+                height: 6,
+                width: 120,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10))),
+                 Spacer(),
+            task.isCompleted == 1
+                ? Container()
+                : _bottomSheetButton(
+              label: "Task Completed",
+              onTap: () {
+                _taskController.markTaskCompleted(task.id!);
+                Get.back();
+              },
+              clr: primaryClr,
+              context: context,
+            ),
+          ])),
+    );
+  }
+
+  _bottomSheetButton(
+      {required String label,
+      required Function()? onTap,
+      required Color clr,
+      bool isClose = false,
+      required BuildContext context}) {
+    return GestureDetector(
+        onTap: onTap,
+        child: Container(
+            margin: const EdgeInsets.symmetric(vertical: 4),
+            height: 55,
+            width: MediaQuery.of(context).size.width * 0.9,
+            decoration: BoxDecoration(
+              border: Border.all(
+                  width: 2,
+                  color: isClose == true
+                      ? Get.isDarkMode
+                          ? Colors.grey[600]!
+                          : Colors.grey[300]!
+                      : clr),
+              borderRadius: BorderRadius.circular(20),
+              color: isClose == true ? Colors.transparent : clr,
+            ),
+            child: Center(
+              child: Text(
+                label,
+                style: isClose
+                    ? titleStyle
+                    : titleStyle.copyWith(color: Colors.white),
+              ),
+            )));
   }
 }
