d5ab5ece88979fa937435fc97341f10144619f3f
Aashay Kulkarni <72932792+aashayk18@users.noreply.github.com>
2023-05-09 22:24:27 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,12 @@ import 'package:flutter_to_do_app/ui/home_page.dart';
 import 'package:flutter_to_do_app/ui/theme.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get_storage/get_storage.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'db/db_helper.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await DBHelper.initDb();
   await GetStorage.init();
   runApp(const MyApp());
